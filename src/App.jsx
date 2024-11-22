8d455dce2da8e8cbcea1278b56b8b38c6e672a26
Antonio Santos <antoniomndsantos@gmail.com>
2024-11-22 11:32:49 +0000

    Can create and save projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
+import {useState} from "react";
+import Sidebar from "./components/Sidebar.jsx";
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+
 function App() {
+  const [projects, setProjects] = useState([]);
+  const [newProject, setNewProject] = useState(false);
+
+  function handleNewProject() {
+    setNewProject(true);
+  }
+
+  function handleCancelNewProject() {
+    setNewProject(false);
+  }
+
+  function handleSetProjects(project) {
+    setProjects(prevProjects => [...prevProjects, project]);
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <Sidebar onNewProject={handleNewProject} projects={projects}/>
+      <div className="w-[35rem] mt-16">
+        {
+          newProject ?
+            <NewProject
+              onCancelNewProject={handleCancelNewProject}
+              onSetProject={(project => handleSetProjects(project))}
+            /> :
+            <NoProjectSelected onNewProject={handleNewProject}/>
+        }
+      </div>
+    </main>
   );
 }
 
