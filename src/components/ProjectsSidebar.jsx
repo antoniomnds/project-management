fb779a47b8d5d94ac28f832b9e4eb24f4de90f76
Antonio Santos <antoniomndsantos@gmail.com>
2024-11-25 17:12:28 +0000

    Extract similar buttons into a custom component

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,15 +1,14 @@
+import Button from "./Button.jsx";
+
 export default function ProjectsSidebar({onNewProject, projectState, onSelectProject}) {
   let projectButtonClassName = "w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800";
 
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
-      <button
-        className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100"
-        onClick={onNewProject}
-      >
+      <Button onClick={onNewProject}>
         + Add Project
-      </button>
+      </Button>
       <ul className="mt-8">
         {projectState.projects.length > 0 && projectState.projects.map(project => (
           <li key={project.title}>
