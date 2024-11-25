6c7452289c6f48c7b7c3fa2bc15a78acc6e98153
Antonio Santos <antoniomndsantos@gmail.com>
2024-11-25 17:12:28 +0000

    Extract similar buttons into a custom component

--- src/components/Project/NoProjectSelected.jsx
+++ src/components/Project/NoProjectSelected.jsx
@@ -1,4 +1,5 @@
 import noProjectsImg from "../../assets/no-projects.png";
+import Button from "../Button.jsx";
 
 export default function NoProjectSelected({onNewProject}) {
   return (
@@ -7,12 +8,9 @@ export default function NoProjectSelected({onNewProject}) {
       <h2 className="text-xl font-bold text-stone-500 my-4">No Project Selected</h2>
       <p className="text-stone-400 mb-4">Select a project or get started with a new one</p>
       <p className="mt-8">
-        <button
-          className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100"
-          onClick={onNewProject}
-        >
+        <Button onClick={onNewProject}>
           Create new project
-        </button>
+        </Button>
       </p>
     </div>
   );
